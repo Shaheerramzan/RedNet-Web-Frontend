534cf9bc670dec9c5e33ebb60751f69fcf0b934b
Shaheer <shaheerramzan1619@gmail.com>
2020-03-25 08:57:09 -0700

    Login Complete

--- src/components/Login/LoginMainPage.js
+++ src/components/Login/LoginMainPage.js
@@ -1,25 +1,56 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Container, Row, Col } from "react-bootstrap";
-import { Form, Field } from "redux-form";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import { Field, reduxForm } from "redux-form";
 
 import Header from "./Header";
 import "./LoginHomepage.css";
 import "./LoginMainpage.css";
 
-class LoginMainPage extends React.Component {
+const renderField = ({ type, label, placeholder, input }) => {
+  return (
+    <Container className="input-container form-group">
+      <label className="white label">{label}</label>
+      <input
+        className="form-control"
+        {...input}
+        type={type}
+        placeholder={placeholder}
+      />
+    </Container>
+  );
+};
 
+class LoginMainPage extends React.Component {
   render() {
     const Name = this.props.location.state.text;
     return (
       <Container fluid className="bg-img">
         <Header ForHomepage={false} />
         <Row>
-          <Col>
+          <Col className="login-col">
             <Container className="login-main-div">
               <div className="login-text-div">
                 <span className="white">{Name}</span>
               </div>
+              <Form className="login-form">
+                <Field
+                  name="name"
+                  placeholder="Enter your Email or Phone Number"
+                  component={renderField}
+                  label="Email / Phone"
+                  type="text"
+                />
+                <Field
+                  name="password"
+                  placeholder="Enter your Password"
+                  component={renderField}
+                  label="Password"
+                  type="password"
+                />
+                <div className="button-div">
+                <button type="submit" className="btn btn-danger">Login</button>
+                </div>
+              </Form>
             </Container>
           </Col>
         </Row>
@@ -28,4 +59,4 @@ class LoginMainPage extends React.Component {
   }
 }
 
-export default connect(null, null)(LoginMainPage);
+export default reduxForm({ form: "LoginForm" })(LoginMainPage);
